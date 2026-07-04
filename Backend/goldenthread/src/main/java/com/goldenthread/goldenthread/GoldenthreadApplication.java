387a73b40aa25e87b50ccbb904074676edd50c93
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-07-05 00:24:39 +0200

    Se agrego el proyecto de backend por Spring Initializr

---
+++ Backend/goldenthread/src/main/java/com/goldenthread/goldenthread/GoldenthreadApplication.java
@@ -0,0 +1,13 @@
+package com.goldenthread.goldenthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenthreadApplication.class, args);
+	}
+
+}
